9464a627c97ba0de3a200e16ac406ea5872d01ce
eishun <shutainer@yahoo.co.jp>
2020-06-21 19:47:13 +0900

    Initial Commit

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by 須藤英隼 on 2020/06/21.
+//  Copyright © 2020 Eishun Sudo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
